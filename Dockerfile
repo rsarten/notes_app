04e3de0cbf5a69624e0a2774059cc8c53a9e7474
Rory <rorysarten@gmail.com>
2021-07-15 22:41:31 +1200

    use gunicorn and set /notes prefix

--- Dockerfile
+++ Dockerfile
@@ -5,4 +5,4 @@ COPY . .
 RUN pip install -r requirements.txt
 
 EXPOSE 5000
-CMD ["flask", "run"]
+CMD ["gunicorn", "--bind", "0.0.0.0:5000", "wsgi:app"]
--- app.py
+++ app.py
@@ -33,3 +33,6 @@ def create():
             return redirect(url_for('index'))
     
     return render_template("create.html")
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
